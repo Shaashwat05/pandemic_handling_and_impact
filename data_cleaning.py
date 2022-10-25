84ca5a822271288166697caf6d0507879003dccd
Shaashwat <shaas2000@gmail.com>
2022-10-24 21:22:37 -0500

    data cleaning final

--- data_cleaning.py
+++ data_cleaning.py
@@ -9,13 +9,15 @@ from sklearn import metrics
 '''
 '''
 KEY OBSERVATIONS
-1. Risk group column highly biased - 77k yes, 27k no, 9k not sure ( normally bias in labels affect model, what about bias in columns?)
+1. Risk group column highly biased - 77k yes, 27k no, 9k not sure 
 2. 66 percent accuracy of the ridge classifier for marital status missing values (tried with employment, gender, education)
 3. isolation adults has 19000 missing values - remove column
 4. isolation children has 20000 missing values - remove column (might need)
 5. OECD_people_2 is dropped as trust in personal relations dont act as a contributing factor to COVID19
 6. Replacing country with their latitudes and longitudes to reduce encoding dimensionality and introducing grographic significance
-7. Mean of each of PSS10, loneliness, SPS and compliance is considered instead of each of the attributes.
+7. Mean of each of PSS10, loneliness, SPS and compliance is considered instead of each of the attributes. 
+    (for rows with NA values division should be by one less number)
+8. SPS Average dropped due to very low correlation with stress and approx 30k missing values
 '''
 
 def clean(df):
@@ -31,7 +33,6 @@ def clean(df):
     df = df[df['Country'].notna()] # 492 rows
     df = df[df['Dem_Expat'].notna()] # 536 rows
     df = df[df['Dem_riskgroup'].notna()] # 455 rows
-    df = df[df['Dem_riskgroup'].notna()] # 455 rows
 
     # Modifying education column values. If employed then education = Up to 12 years of School
     df.loc[df["Dem_edu"] == 'Uninformative response', "Dem_edu"] = np.nan
@@ -41,7 +42,6 @@ def clean(df):
     # For employment NA values, if age > avg global retirement age(62, acording to CNBC) then retired, else, drop data (1000 rows)
     df.loc[df["Dem_employment"].isna() & df["Dem_age"] >= 62, "Dem_employment"] = "Retired"
     df = df[df['Dem_employment'].notna()]
-    #print(df.loc[df['Dem_gender'].isna()])
 
     # Missing value handling for isolation - whenever isolation_adults in NA, isolation - life carries on
     df.loc[df["Dem_islolation"].isna() & df["Dem_isolation_adults"].isna(), "Dem_islolation"] = "Life carries on with minor changes"
@@ -55,10 +55,25 @@ def clean(df):
     #pred = lr.predict(dummy_df)
     #print(metrics.accuracy_score(y, pred))
 
+    # Lon_avg - 8289 missing values, 8044 common missing values with trust and PSS - deleting them
+    df.loc[df["PSS10_avg"].isna(), ["Trust_countrymeasure", "Lon_avg"]] = "del"
+    #df["Trust_countrymeasure"] = df["Trust_countrymeasure"].astype(float)
+    df = df[df["Trust_countrymeasure"] != "del"]
+    df.loc[df["Trust_countrymeasure"].isna(), "Trust_countrymeasure"] = df["Trust_countrymeasure"].mean() # mean trust value
+    df = df[df["Lon_avg"] != "del"]
+    df.loc[df["Lon_avg"].isna(), "Lon_avg"] = df["Lon_avg"].mean() # mean lonliness value
+    df = df[df['PSS10_avg'].notna()]
+
+    # Expl distresss convertiing 99 to 7 ( Does not apply to my situation )
+    for i in range(24):
+        df.loc[df["Expl_Distress_"+str(i+1)] == 99, "Expl_Distress_"+str(i+1)] = 7
+
     # Dropping Additional Columns after preprocessing
     df.drop(["Dem_isolation_adults", "OECD_people_2"], axis=1, inplace=True)
 
     return df
+
+
 
 def selection_alteration(df):
     # Replacing country with their latitudes and longitudes to reduce encoding dimensionality and introducing grographic significance
@@ -67,10 +82,11 @@ def selection_alteration(df):
     df = df.merge(country_pos, how="left", left_on="Country", right_on="country")
 
     # Selecting only those columns that are required
-    df_columns = ["PSS10_avg", "latitude", "longitude", "Trust_countrymeasure", "SPS_avg", "lon_avg"]
+    df_columns = ["PSS10_avg", "latitude", "longitude", "Trust_countrymeasure", "Lon_avg"]
     df_columns += [c for c in df.columns if "Dem_" in c]
     df_columns += [c for c in df.columns if "Corona" in c]
     df_columns += [c for c in df.columns if "Expl_Distress" in c]
+
     try:
         df_columns.remove("Expl_Distress_txt")
     except ValueError:
@@ -83,10 +99,17 @@ def selection_alteration(df):
     df.drop([i for i in df_columns if "Compliance" in i], axis="columns", inplace=True)
     df["Compliance"] = compliance
 
+    # Dropping all rows with remaining NA values
+    df = df.dropna()
+
     return df
+
+
 
 if __name__=="__main__":
     df = pd.read_csv("COVIDiSTRESS_April_27_clean.csv", encoding= 'unicode_escape')
     df = clean(df)
     df = selection_alteration(df)
-    print(df['Dem_edu'].head(50))
+    print(df.head())
+    print(df.shape[0])
+    df.to_csv("out.csv")
